4ac51667acbb2719cde826e81bf370d989099376
chetanmehar <119054386+chetanmehar@users.noreply.github.com>
2024-08-04 15:06:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,8 +51,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://chetanmehar581:cURMqNaihtrikHwR@meharmovie.v4zvytq.mongodb.net/?retryWrites=true&w=majority&appName=Meharmovie")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Meharmovie")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://harisin2008s:8uIRrM2BXf43aFEo@cluster0.ebmu6vh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
 # Premium And Referal Settings
